f8ce28c16541116edbdc48048046e0672a9cd335
tahaamin2013 <tahaamindob2013@gmail.com>
2024-06-22 17:07:14 +0500

    fds

--- src/app/manifest.tsx
+++ src/app/manifest.tsx
@@ -1,33 +1,33 @@
 export default function manifest() {
     return {
-      name: 'Starbucks Menu With Prices',
-      short_name: 'Starbucks',
-      description: 'Next.js App',
-      start_url: '/',
-      display: 'standalone',
-    //   background_color: '#fff',
-    //   theme_color: '#fff',
+      name: "Starbucks Menu With Prices",
+      short_name: "Starbucks",
+      description: "Next.js App",
+      start_url: "/",
+      display: "standalone",
+      //   background_color: '#fff',
+      //   theme_color: '#fff',
       icons: [
         {
-          src: '/favicon-32x32.png',
-          sizes: '32x32',
-          type: 'image/png',
+          src: "/icon.ico",
+          sizes: "32x32",
+          type: "image/png",
         },
-          {
-          src: '/favicon-16x16.png',
-          sizes: '16x16',
-          type: 'image/png',
+        {
+          src: "/icon.ico",
+          sizes: "16x16",
+          type: "image/png",
         },
-            {
-          src: '/android-chrome-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
+        {
+          src: "/icon.ico",
+          sizes: "192x192",
+          type: "image/png",
         },
-            {
-          src: '/android-chrome-512x512',
-          sizes: '512x512',
-          type: 'image/png',
+        {
+          src: "/icon.ico",
+          sizes: "512x512",
+          type: "image/png",
         },
       ],
-    }
+    };
   }
--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -8,13 +8,6 @@ const siteMetadata = {
   siteUrl: "https://starbucks-menu-with-prices.net/", // your website URL
   siteLogo: "/icon.ico",
   socialBanner: "/socialBanner.webp", // add social banner in the public folder
-  email: "aimahusnain@gmail.com",
-  github: "https://github.com/aimahusnain",
-  twitter: "https://twitter.com/aimahusnain",
-  facebook: "https://facebook.com",
-  youtube: "https://youtube.com/aimahusnain",
-  linkedin: "https://www.linkedin.com/in/aimahusnain/",
-  dribbble: "https://www.dribbble.com/aimahusnain",
   locale: "en-US",
 };
 
