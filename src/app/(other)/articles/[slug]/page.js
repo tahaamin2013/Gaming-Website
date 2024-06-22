dab247727f16b6b6efb6f8d25719a9f54672d925
tahaamin2013 <tahaamindob2013@gmail.com>
2024-06-22 23:06:44 +0500

    dd

--- src/app/(other)/articles/[slug]/page.js
+++ src/app/(other)/articles/[slug]/page.js
@@ -1,61 +1,7 @@
-import BlogReader from "@/src/components/Blog/BlogReader/BlogReading";
-import ScrollProgressBar from '@/src/components/Blog/BlogReader/ScrollProgressBar';
-import siteMetadata from "@/src/utils/siteMetaData";
-import { allBlogs } from "contentlayer/generated";
+import dynamic from 'next/dynamic';
+const Floating_Bar = dynamic(() => import('@/src/components/Blog/BlogReader/Floating_Bar'), { ssr: false });
 
-export async function generateStaticParams() {
-  return allBlogs.map((blog) => ({ slug: blog._raw.flattenedPath }));
-}
-
-export async function generateMetadata({ params }) {
-  const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
-  if (!blog) {
-    return;
-    
-  }
-
-  const publishedAt = new Date(blog.publishedAt).toISOString();
-  const modifiedAt = new Date(blog.updatedAt || blog.publishedAt).toISOString();
-
-  let imageList = [siteMetadata.socialBanner];
-  if (blog.image) {
-    imageList =
-      typeof blog.image.filePath === "string"
-        ? [siteMetadata.siteUrl + blog.image.filePath.replace("../public", "")]
-        : blog.image;
-  }
-  const ogImages = imageList.map((img) => {
-    return { url: img.includes("http") ? img : siteMetadata.siteUrl + img };
-  });
-
-  const authors = blog?.author ? [blog.author] : siteMetadata.author;
-
-  return {
-    title: blog.title,
-    description: blog.description,
-    openGraph: {
-      title: blog.title,
-      description: blog.description,
-      url: siteMetadata.siteUrl + blog.url,
-      siteName: siteMetadata.title,
-      locale: "en_US",
-      type: "article",
-      publishedTime: publishedAt,
-      modifiedTime: modifiedAt,
-      images: ogImages,
-      authors: authors.length > 0 ? authors : [siteMetadata.author],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: blog.title,
-      description: blog.description,
-      images: ogImages,
-    },
-  };
-}
-
-import React from 'react'
-
+// Inside your BlogPage component
 const BlogPage = ({ params }) => {
   const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
 
@@ -86,16 +32,17 @@ const BlogPage = ({ params }) => {
 
   return (
     <>
-        <ScrollProgressBar />
+      <ScrollProgressBar />
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
       <article>
         <BlogReader parmy={params} blogy={blog} />
+        <Floating_Bar />
       </article>
     </>
   );
 }
 
-export default BlogPage
+export default BlogPage;
