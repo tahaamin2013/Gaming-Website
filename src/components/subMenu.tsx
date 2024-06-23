870bdae3ca72361f0f6a8426431debaa3f025771
tahaamin2013 <tahaamindob2013@gmail.com>
2024-06-23 22:12:24 +0500

    remove scroll rounded effect in sub menu

--- src/components/subMenu.tsx
+++ src/components/subMenu.tsx
@@ -24,15 +24,11 @@ const SubMenu = () => {
 
   return (
     <div
-      className={`border-2 text-center transition-all duration-500 bg-[#f9f9f9] px-9 py-3 sticky top-0 z-40 ${
-        rounded ? "rounded-full mx-auto top-[10px] w-fit" : ""
-      }`}
+      className='border-2 text-center transition-all duration-500 bg-[#f9f9f9] px-9 py-3 sticky top-0 z-40'
     >
       <div
-        className={`flex gap-8 transition-all duration-500 font-bold text-sm w-full justify-center sm:justify-start ${
-          rounded ? " w-fit sm:justify-center" : ""
-        }`}
-      >
+        className='flex gap-8 transition-all duration-500 font-bold text-sm w-full justify-center sm:justify-start'
+      > 
         <Link href="/">Menu</Link>
         <Link href="/featured">Featured</Link>
       </div>
